d9cd11826ef3bdb206a7ac3f39ce3d09d9240dcb
Mark A. Hershberger <mah@everybody.org>
2014-09-10 22:07:09 -0400

    Don't check for Template:Extension_DPL during update
    The check causes MediaWiki to execute some code that depends on some
    updates having been run, which is no good.
    
    bug: 60492
    Change-Id: I4305eb1db19005aa09c6ea76086f402aba41bade

--- DPLSetup.php
+++ DPLSetup.php
@@ -1228,6 +1228,7 @@ class ExtDynamicPageList {
                 'resetCategories' => false, 'resetImages' => false, 'resetdone' => false , 'elimdone' => false );
         }
 
+		if ( !defined( "MW_UPDATER" ) ) {
 		// make sure page "Template:Extension DPL" exists
         $title = Title::newFromText('Template:Extension DPL');
 		global $wgUser;
@@ -1239,7 +1240,7 @@ class ExtDynamicPageList {
 							  $title, EDIT_NEW | EDIT_FORCE_BOT );
 			die(header('Location: '.Title::newFromText('Template:Extension DPL')->getFullURL()));
 		}
-
+		}
         require_once( 'DPLVariables.php' );
 	}
 
