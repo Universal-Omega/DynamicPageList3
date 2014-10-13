8ad2ce0210fa96de6ad31304874ffd60909ce156
Alexia E. Smith <washuu@gmail.com>
2014-10-13 13:58:37 -0500

    Code pass clean up on ListMode and add in missing break statements.

--- classes/ListMode.php
+++ classes/ListMode.php
@@ -7,7 +7,7 @@
  * @license		GPL
  * @package		DynamicPageList
  *
-**/
+ **/
 namespace DPL;
 
 class ListMode {
@@ -44,65 +44,73 @@ class ListMode {
 			$inlinetext = '&#160;-&#160;';
 		}
 		$this->name = $listmode;
-		$_listattr = ($listattr == '') ? '' : ' ' . Sanitizer::fixTagAttributes( $listattr, 'ul' );
-		$_itemattr = ($itemattr == '') ? '' : ' ' . Sanitizer::fixTagAttributes( $itemattr, 'li' );
+		$_listattr  = ($listattr == '') ? '' : ' '.Sanitizer::fixTagAttributes($listattr, 'ul');
+		$_itemattr  = ($itemattr == '') ? '' : ' '.Sanitizer::fixTagAttributes($itemattr, 'li');
 		
-		$this->sSectionTags = $sectionSeparators;
+		$this->sSectionTags        = $sectionSeparators;
 		$this->aMultiSecSeparators = $multiSectionSeparators;
-		$this->iDominantSection = $dominantSection - 1;  // 0 based index
+		$this->iDominantSection    = $dominantSection - 1; // 0 based index
 		
 		switch ($listmode) {
 			case 'inline':
-				if( stristr($inlinetext, '<BR />') ) { //one item per line (pseudo-inline)
-					$this->sListStart = '<DIV'. $_listattr . '>';
-					$this->sListEnd = '</DIV>';
+				if (stristr($inlinetext, '<BR />')) { //one item per line (pseudo-inline)
+					$this->sListStart = '<DIV'.$_listattr.'>';
+					$this->sListEnd   = '</DIV>';
 				}
-				$this->sItemStart = '<SPAN' . $_itemattr . '>';
-				$this->sItemEnd = '</SPAN>';
-				$this->sInline = $inlinetext;
+				$this->sItemStart = '<SPAN'.$_itemattr.'>';
+				$this->sItemEnd   = '</SPAN>';
+				$this->sInline    = $inlinetext;
 				break;
 			case 'ordered':
-				if ($iOffset==0) $this->sListStart = '<OL start=1 ' . $_listattr . '>';
-				else 			 $this->sListStart = '<OL start=' . ($iOffset+1) . ' ' . $_listattr . '>';
-				$this->sListEnd = '</OL>';
-				$this->sItemStart = '<LI'. $_itemattr . '>';
-				$this->sItemEnd = '</LI>';
+				if ($iOffset == 0) {
+					$this->sListStart = '<OL start=1 '.$_listattr.'>';
+				} else {
+					$this->sListStart = '<OL start='.($iOffset + 1).' '.$_listattr.'>';
+				}
+				$this->sListEnd   = '</OL>';
+				$this->sItemStart = '<LI'.$_itemattr.'>';
+				$this->sItemEnd   = '</LI>';
 				break;
 			case 'unordered':
-				$this->sListStart = '<UL' . $_listattr . '>';
-				$this->sListEnd = '</UL>';
-				$this->sItemStart = '<LI' . $_itemattr . '>';
-				$this->sItemEnd = '</LI>';
+				$this->sListStart = '<UL'.$_listattr.'>';
+				$this->sListEnd   = '</UL>';
+				$this->sItemStart = '<LI'.$_itemattr.'>';
+				$this->sItemEnd   = '</LI>';
 				break;
 			case 'definition':
-				$this->sListStart = '<DL' . $_listattr . '>';
-				$this->sListEnd = '</DL>';
+				$this->sListStart    = '<DL'.$_listattr.'>';
+				$this->sListEnd      = '</DL>';
 				// item html attributes on dt element or dd element ?
 				$this->sHeadingStart = '<DT>';
-				$this->sHeadingEnd = '</DT><DD>';
-				$this->sItemEnd = '</DD>';
+				$this->sHeadingEnd   = '</DT><DD>';
+				$this->sItemEnd      = '</DD>';
 				break;
 			case 'H2':
 			case 'H3':
 			case 'H4':
-				$this->sListStart = '<DIV' . $_listattr . '>';
-				$this->sListEnd = '</DIV>';
-				$this->sHeadingStart = '<' . $listmode .'>';
-				$this->sHeadingEnd = '</' . $listmode . '>';
+				$this->sListStart    = '<DIV'.$_listattr.'>';
+				$this->sListEnd      = '</DIV>';
+				$this->sHeadingStart = '<'.$listmode.'>';
+				$this->sHeadingEnd   = '</'.$listmode.'>';
 				break;
 			case 'userformat':
-				switch(count($listseparators)) {
+				switch (count($listseparators)) {
 					case 4:
 						$this->sListEnd = $listseparators[3];
+						break:
 					case 3:
 						$this->sItemEnd = $listseparators[2];
+						break:
 					case 2:
 						$this->sItemStart = $listseparators[1];
+						break:
 					case 1:
 						$this->sListStart = $listseparators[0];
+						break:
 				}
 				$this->sInline = $inlinetext;
 				break;
 		}
 	}
 }
+?>
