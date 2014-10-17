9cd01d03982da7d4082f260c2e18d1f8cfba5b9f
Alexia E. Smith <washuu@gmail.com>
2014-10-17 11:06:39 -0500

    New Config class for handling DPL's many configuration settings.

--- DynamicPageList.php
+++ DynamicPageList.php
@@ -40,16 +40,16 @@ $wgMessagesDirs['DynamicPageList']					= "{$extDir}/i18n";
 $wgExtensionMessagesFiles['DynamicPageList']		= "{$extDir}/DynamicPageList.i18n.php";
 $wgExtensionMessagesFiles['DynamicPageListMagic']	= "{$extDir}/DynamicPageList.i18n.magic.php";
 
-$wgAutoloadClasses['DynamicPageListHooks']			= "{$extDir}/DPLSetup.php";
+$wgAutoloadClasses['DynamicPageListHooks']			= "{$extDir}/DynamicPageList.hooks.php";
 $wgAutoloadClasses['DPL\Article']					= "{$extDir}/classes/Article.php";
 $wgAutoloadClasses['DPL\DynamicPageList']			= "{$extDir}/classes/DynamicPageList.php";
 $wgAutoloadClasses['DPL\Logger']					= "{$extDir}/classes/Logger.php";
 $wgAutoloadClasses['DPL\ListMode']					= "{$extDir}/classes/ListMode.php";
 $wgAutoloadClasses['DPL\Logger']					= "{$extDir}/classes/Logger.php";
 $wgAutoloadClasses['DPL\LST']						= "{$extDir}/classes/LST.php";
-$wgAutoloadClasses['DPL\Main']						= "{$extDir}/classes/Main.php";
 $wgAutoloadClasses['DPL\Parameters']				= "{$extDir}/classes/Parameters.php";
 $wgAutoloadClasses['DPL\ParametersData']			= "{$extDir}/classes/ParametersData.php";
+$wgAutoloadClasses['DPL\Parse']						= "{$extDir}/classes/Parse.php";
 $wgAutoloadClasses['DPL\Query']						= "{$extDir}/classes/Query.php";
 $wgAutoloadClasses['DPL\Variables']					= "{$extDir}/classes/Variables.php";
 
@@ -70,10 +70,76 @@ if (!isset($wgGroupPermissions['sysop']['dpl_param_delete_rules'])) {
 /******************************************/
 /* Final Setup                            */
 /******************************************/
-if (isset($dplMigrationTesting) && $dplMigrationTesting === true) {
-	//Use full functionality by default.
-	\DPL\ParametersData::setRichness(0);
-} else {
-	\DPL\ParametersData::setRichness(4);
+
+//By default all setup namespaces are used when DPL initializes.  Customize this setting with an array of namespace constants to restrict DPL to work only in those namespaces.
+/* Example, restrict DPL to look only in the Main and Project namespaces.
+	$dplSettings['allowedNamespaces'] = [
+		NS_MAIN,
+		NS_PROJECT
+	];
+*/
+if (!isset($dplSettings['allowedNamespaces']) {
+	$dplSettings['allowedNamespaces'] = null;
 }
+
+//Set this to true to ignore 'maxCategoryCount' and allow unlimited categories.  Please note that large amounts of categories in a query can slow down or crash servers.
+if (!isset($dplSettings['allowUnlimitedCategories']) {
+	$dplSettings['allowUnlimitedCategories'] = false;
+}
+
+//Set this to true to ignore 'maxResultCount' and allow unlimited results.  Please note that large result sets may result in slow or failed page loads.
+if (!isset($dplSettings['allowUnlimitedResults']) {
+	$dplSettings['allowUnlimitedResults'] = false;
+}
+
+//Set DPL to always behave like intersection.
+if (!isset($dplSettings['behavingLikeIntersection']) {
+	$dplSettings['behavingLikeIntersection'] = false;
+}
+
+//Maximum number of items in a category list before being cut off.
+if (!isset($dplSettings['categoryStyleListCutoff']) {
+	$dplSettings['categoryStyleListCutoff'] = 6;
+}
+
+//This does something with preventing DPL from "looking" at these categories.  @TODO: I will figure this out later.
+if (!isset($dplSettings['fixedCategories']) {
+	$dplSettings['fixedCategories'] = [];
+}
+
+//Set the level of parameters available to end users.
+if (!isset($dplSettings['functionalRichness']) {
+	if (isset($dplMigrationTesting) && $dplMigrationTesting === true) {
+		$dplSettings['functionalRichness'] = 0;
+	} else {
+		$dplSettings['functionalRichness'] = 3;
+	}
+}
+
+//Maximum number of categories to allow in queries.
+if (!isset($dplSettings['maxCategoryCount']) {
+	$dplSettings['maxCategoryCount'] = 4;
+}
+
+//Maximum number of categories to allow in queries.  I guess this is needed somewhere?
+if (!isset($dplSettings['minCategoryCount']) {
+	$dplSettings['minCategoryCount'] = 0;
+}
+
+//Maximum number of results to return from a query.
+if (!isset($dplSettings['maxResultCount']) {
+	$dplSettings['maxResultCount'] = 500;
+}
+
+//Set this to true to allow DPL to run from protected pages only.
+if (!isset($dplSettings['runFromProtectedPagesOnly']) {
+	$dplSettings['runFromProtectedPagesOnly'] = false;
+}
+
+//Force DPL to respect the parser cache.  It makes DPL less dynamic, but reduces overall server load.
+if (!isset($dplSettings['respectParserCache']) {
+	$dplSettings['respectParserCache'] = false;
+}
+
+\DPL\Config::init($dplSettings);
 ?>
---
+++ classes/Config.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * DynamicPageList
+ * DPL Config Class
+ *
+ * @author		IlyaHaykinson, Unendlich, Dangerville, Algorithmix, Theaitetos, Alexia E. Smith
+ * @license		GPL
+ * @package		DynamicPageList
+ *
+ **/
+namespace DPL;
+
+class Config {
+	/**
+	 * Configuration Settings
+	 *
+	 * @var		array
+	 */
+	static private $settings = [];
+
+	/**
+	 * Initialize the static object with settings.
+	 *
+	 * @access	public
+	 * @return	void
+	 */
+	static public function init($settings) {
+		if (!is_array($settings)) {
+			throw new MWException(__METHOD__.": Invalid settings passed.");
+		}
+		self::$settings = array_merge(self::$settings, $settings);
+	}
+
+	/**
+	 * Return a single setting.
+	 *
+	 * @access	public
+	 * @param	string	Setting Key
+	 * @return	mixed	The setting's actual setting or null if it does not exist.
+	 */
+	static public function getSetting($setting) {
+		return (array_key_exists($setting, self::$settings ? self::$settings[$setting] : null));
+	}
+
+	/**
+	 * Return a all settings.
+	 *
+	 * @access	public
+	 * @return	array	All settings
+	 */
+	static public function getAllSettings() {
+		return self::$settings;
+	}
+
+	/**
+	 * Set a single setting.
+	 *
+	 * @access	public
+	 * @param	string	Setting Key
+	 * @param	mixed	[Optional] Appropriate value for the setting key.
+	 * @return	void
+	 */
+	static public function setSetting($setting, $value = null) {
+		if (empty($setting) || !is_string($setting)) {
+			throw new MWException(__METHOD__.": Setting keys can not be blank.");
+		}
+		self::$settings[$setting] = $value;
+	}
+}
+?>
