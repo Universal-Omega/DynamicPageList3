433e3a9757cbdf9406559eb1c519ae46e7f8aa0d
Alexia E. Smith <washuu@gmail.com>
2014-10-21 18:49:40 -0500

    Coding standards.

--- classes/Article.php
+++ classes/Article.php
@@ -73,15 +73,15 @@ class Article {
 
 		//chop off title if "too long"
 		if (is_numeric($parameters->getParameter('titlemaxlen')) && strlen($titleText) > $parameters->getParameter('titlemaxlen')) {
-			$titleText = substr($titleText, 0, $parameters->getParameter('titlemaxlen')) . '...';
+			$titleText = substr($titleText, 0, $parameters->getParameter('titlemaxlen')).'...';
 		}
 		if ($parameters->getParameter('showcurid') && isset($row['page_id'])) {
-			$articleLink = '[{{fullurl:' . $title->getText() . '|curid=' . $row['page_id'] . '}} ' . htmlspecialchars($titleText) . ']';
+			$articleLink = '[{{fullurl:'.$title->getText().'|curid='.$row['page_id'].'}} '.htmlspecialchars($titleText).']';
 		} else if (!$parameters->getParameter('escapelinks') || ($pageNamespace != NS_CATEGORY && $pageNamespace != NS_FILE)) {
 			// links to categories or images need an additional ":"
-			$articleLink = '[[' . $title->getPrefixedText() . '|' . $wgContLang->convert($titleText) . ']]';
+			$articleLink = '[['.$title->getPrefixedText().'|'.$wgContLang->convert($titleText).']]';
 		} else {
-			$articleLink = '[{{fullurl:' . $title->getText() . '}} ' . htmlspecialchars($titleText) . ']';
+			$articleLink = '[{{fullurl:'.$title->getText().'}} '.htmlspecialchars($titleText).']';
 		}
 
 		$article->mLink = $articleLink;
@@ -177,7 +177,7 @@ class Article {
 			// because we are going to do a recursive parse at the end of the output phase
 			// we have to generate wiki syntax for linking to a user´s homepage
 			if ($parameters->getParameter('adduser') || $parameters->getParameter('addauthor') || $parameters->getParameter('addlasteditor') || $parameters->getParameter('lastrevisionbefore') || $parameters->getParameter('allrevisionsbefore') || $parameters->getParameter('firstrevisionsince') || $parameters->getParameter('allrevisionssince')) {
-				$article->mUserLink = '[[User:' . $row['rev_user_text'] . '|' . $row['rev_user_text'] . ']]';
+				$article->mUserLink = '[[User:'.$row['rev_user_text'].'|'.$row['rev_user_text'].']]';
 				$article->mUser     = $row['rev_user_text'];
 				$article->mComment  = $row['rev_comment'];
 			}
@@ -186,7 +186,7 @@ class Article {
 			if ($parameters->getParameter('addcategories') && ($row['cats'] != '')) {
 				$artCatNames = explode(' | ', $row['cats']);
 				foreach ($artCatNames as $artCatName) {
-					$article->mCategoryLinks[] = '[[:Category:' . $artCatName . '|' . str_replace('_', ' ', $artCatName) . ']]';
+					$article->mCategoryLinks[] = '[[:Category:'.$artCatName.'|'.str_replace('_', ' ', $artCatName).']]';
 					$article->mCategoryTexts[] = str_replace('_', ' ', $artCatName);
 				}
 			}
@@ -198,18 +198,18 @@ class Article {
 						$headings[$row['cl_to']] = isset($headings[$row['cl_to']]) ? $headings[$row['cl_to']] + 1 : 1;
 						if ($row['cl_to'] == '') {
 							//uncategorized page (used if ordermethod=category,...)
-							$article->mParentHLink = '[[:Special:Uncategorizedpages|' . wfMsg('uncategorizedpages') . ']]';
+							$article->mParentHLink = '[[:Special:Uncategorizedpages|'.wfMsg('uncategorizedpages').']]';
 						} else {
-							$article->mParentHLink = '[[:Category:' . $row['cl_to'] . '|' . str_replace('_', ' ', $row['cl_to']) . ']]';
+							$article->mParentHLink = '[[:Category:'.$row['cl_to'].'|'.str_replace('_', ' ', $row['cl_to']).']]';
 						}
 						break;
 					case 'user':
 						$headings[$row['rev_user_text']] = isset($headings[$row['rev_user_text']]) ? $headings[$row['rev_user_text']] + 1 : 1;
 						if ($row['rev_user'] == 0) { //anonymous user
-							$article->mParentHLink = '[[User:' . $row['rev_user_text'] . '|' . $row['rev_user_text'] . ']]';
+							$article->mParentHLink = '[[User:'.$row['rev_user_text'].'|'.$row['rev_user_text'].']]';
 
 						} else {
-							$article->mParentHLink = '[[User:' . $row['rev_user_text'] . '|' . $row['rev_user_text'] . ']]';
+							$article->mParentHLink = '[[User:'.$row['rev_user_text'].'|'.$row['rev_user_text'].']]';
 						}
 						break;
 				}
