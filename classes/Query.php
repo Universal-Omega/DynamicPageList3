aa6df5a561631c8ba43cfb574007b0683d43c0e6
Alexia E. Smith <washuu@gmail.com>
2014-12-10 13:55:52 -0600

    Don't leave var_dump()s in your code.

--- classes/Query.php
+++ classes/Query.php
@@ -251,7 +251,7 @@ class Query {
 		wfProfileOut(__METHOD__.": Query Build");
 
 		wfProfileIn(__METHOD__.": Database Query");
-		var_dump($sql);
+
 		$queryError = false;
 		try {
 			$result = $this->DB->query($sql);
