5890d75cc1406880ec0f0f9dba5a68984d0f284d
Alexia E. Smith <washuu@gmail.com>
2015-05-07 15:21:41 -0500

    Version bump.

--- DynamicPageList.php
+++ DynamicPageList.php
@@ -16,7 +16,7 @@ if (!defined('MEDIAWIKI')) {
 /******************************************/
 /* Credits								  */
 /******************************************/
-define('DPL_VERSION', '3.0.0');
+define('DPL_VERSION', '3.0.1');
 
 $credits = [
 	'path' 				=> __FILE__,
