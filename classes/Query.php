4be3081fd43c1e7133321ee61bbbdb66cd74fd13
Jack Phoenix <ashley@uncyclomedia.co>
2019-04-03 20:03:40 +0000

    Replication group readiness for replication setups
    So that it's possible to set up a dedicated slave for DPL queries, which can be quite problematic in certain cases (e.g. filesort *and* join buffer _in the same query_).

--- classes/Query.php
+++ classes/Query.php
@@ -156,7 +156,7 @@ class Query {
 
 		$this->tableNames = self::getTableNames();
 
-		$this->DB = wfGetDB(DB_REPLICA);
+		$this->DB = wfGetDB(DB_REPLICA, 'dpl');
 	}
 
 	/**
@@ -349,7 +349,7 @@ class Query {
 	 * @return	array	Prepared table names.
 	 */
 	public static function getTableNames() {
-		$DB = wfGetDB(DB_REPLICA);
+		$DB = wfGetDB(DB_REPLICA, 'dpl');
 		$tables = [
 			'categorylinks',
 			'dpl_clview',
@@ -591,7 +591,7 @@ class Query {
 	 * @return	array	Subcategories
 	 */
 	public static function getSubcategories($categoryName, $depth = 1) {
-		$DB = wfGetDB(DB_REPLICA);
+		$DB = wfGetDB(DB_REPLICA, 'dpl');
 
 		if ($depth > 2) {
 			//Hard constrain depth because lots of recursion is bad.
