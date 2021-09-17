1353cd4572e9ebfb206772e2b05758717ee30dce
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-16 20:34:27 -0600

    Update config.php

--- .phan/config.php
+++ .phan/config.php
@@ -2,6 +2,18 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
+$cfg['directory_list'] = array_merge(
+	$cfg['directory_list'], [
+		'../../extensions/Variables',
+	]
+);
+
+$cfg['exclude_analysis_directory_list'] = array_merge(
+	$cfg['exclude_analysis_directory_list'], [
+		'../../extensions/Variables',
+	]
+);
+
 $cfg['suppress_issue_types'] = [
 	'PhanUndeclaredTypeReturnType',
 	'PhanPossiblyUndeclaredVariable',
