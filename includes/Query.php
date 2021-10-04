f7f6b651ce13e70b7efca2f4e53d8abe6c1fcadc
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-10-04 17:33:52 -0600

    Update Query.php

--- includes/Query.php
+++ includes/Query.php
@@ -897,10 +897,12 @@ class Query {
 	 * @param mixed $option
 	 */
 	private function _adduser( $option ) {
+		$actorQuery = $this->actorMigration->getJoin( 'rev_user' );
+		$sortField = $actorQuery['fields']['rev_actor'] !== 'NULL' ? 'rev_actor' : 'rev_user_text';
 		$this->addTable( 'revision', 'rev' );
 		$this->addSelect(
 			[
-				'rev.rev_actor',
+				'rev_actor' => $actorQuery['fields']['rev_actor'],
 			]
 		);
 	}
