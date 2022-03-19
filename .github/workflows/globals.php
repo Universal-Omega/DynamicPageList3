6fe6e0c7793b6d60b24a3986a9d1128424e22cd0
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-03-19 14:59:46 -0600

    Update globals.php

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -1,12 +1,3 @@
 <?php
-$wgMessageCacheType = CACHE_NONE;
-$wgHooks['MediaWikiServices'][] = 'wfOnMediaWikiServices';
 
-function wfOnMediaWikiServices( MediaWiki\MediaWikiServices $services ) {
-	global $wgMemCachedServers, $wgMemCachedPersistent;
-
-	if ( extension_loaded( 'memcached' ) ) {
-		$wgMemCachedServers = [ '0.0.0.0:11211' ];
-		$wgMemCachedPersistent = false;
-	}
-}
+$wgMessageCacheType = CACHE_ACCEL;
