735946409c09a81ba316bf9976e6f0e1f2cc2777
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-03-19 15:13:26 -0600

    Update globals.php

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -1,4 +1,13 @@
 <?php
 
+$wgObjectCaches['redis'] = [
+	'class' => RedisBagOStuff::class,
+	'servers' => [ '127.0.0.1:6379' ],
+	'persistent' => true,
+	'loggroup' => 'redis',
+	'reportDupes' => false,
+	'connectTimeout' => 2,
+];
+
 $wgMessageCacheType = CACHE_ACCEL;
-$wgSessionCacheType = CACHE_ACCEL;
+$wgSessionCacheType = 'redis';
