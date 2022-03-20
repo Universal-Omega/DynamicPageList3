4fbf0b41db5fb671bfffbcf07184dc489ddb2f83
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-03-19 20:51:36 -0600

    Delete globals.php

--- .github/workflows/globals.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$wgHooks['MediaWikiServices'][] = 'wfOnMediaWikiServices';
-
-function wfOnMediaWikiServices( MediaWiki\MediaWikiServices $services ) {
-	global $wgMainCacheType;
-
-	$wgMainCacheType = CACHE_NONE;
-}
