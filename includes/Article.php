7759cc7677e683d6819eef531f64d10e3a8799d1
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-03-24 16:28:08 -0600

    Update Article.php

--- includes/Article.php
+++ includes/Article.php
@@ -290,7 +290,7 @@ class Article {
 				$article->mUser = $revActorName;
 				$article->mDate = $row->revactor_timestamp;
 
-				$row->rev_id = $row->revactor_rev;
+				$row->rev_id = $row->revcomment_rev;
 				$row->comment_id = $row->revcomment_comment_id;
 				// $row->rev_actor = $row->revactor_actor;
 				// $row->rev_user_text = $revActorName;
