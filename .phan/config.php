611a900967f1936a0667cb4804d032632893683a
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-01-14 09:44:12 -0700

    phan: enable `SecurityCheck-ReDoS`
    Usually these are false positives, and I fixed the real ones, but let's try to get them to disappear anyway to be extra safe.

--- .phan/config.php
+++ .phan/config.php
@@ -15,7 +15,6 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 );
 
 $cfg['suppress_issue_types'] = [
-	'SecurityCheck-ReDoS',
 	'SecurityCheck-LikelyFalsePositive',
 	'PhanAccessMethodInternal',
 ];
