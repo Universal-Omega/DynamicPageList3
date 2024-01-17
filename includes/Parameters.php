eea36a87d84e51f3c949c4d9e8fb38794a2eecb2
Tobiasz Musiał <tmusial@fandom.com>
2024-01-17 20:01:25 +0100

    Fix `PHP Warning: Trying to access array offset on value of type null` (#253)

--- includes/Parameters.php
+++ includes/Parameters.php
@@ -957,7 +957,7 @@ class Parameters extends ParametersData {
 	public function _nottitleregexp( $option ) {
 		$data = $this->getParameter( 'nottitle' );
 
-		if ( !is_array( $data['regexp'] ) ) {
+		if ( !$data || !is_array( $data['regexp'] ) ) {
 			$data['regexp'] = [];
 		}
 
