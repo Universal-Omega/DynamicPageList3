2c12508823a559bef02ae64382bbcfa59e5086e3
Ádám Domonkos <78816259+domino-1@users.noreply.github.com>
2024-05-24 00:09:57 +0200

    Update includes/Lister/Lister.php
    Co-authored-by: CosmicAlpha <cosmic.alpha24@gmail.com>

--- includes/Lister/Lister.php
+++ includes/Lister/Lister.php
@@ -1322,7 +1322,6 @@ class Lister {
 	public function getPageImage( int $pageID ) {
 
 		$dbr = MediaWikiServices::getInstance()->getDBLoadBalancer()->getConnection( DB_REPLICA );
-		$dbr = $dbl->getConnection( DB_REPLICA );
 		// In the future, a check could be made for page_image too, but page_image_free is the default, should do for now
 		$propValue = $dbr->selectField(
 			// Table to use
