1e5f8c17873e41ecf41fabfc9f1ffe786a8515b6
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-02-09 14:58:23 -0700

    Try

--- includes/Lister/Lister.php
+++ includes/Lister/Lister.php
@@ -805,7 +805,7 @@ class Lister {
 		} */
 
 		$pageProps = MediaWikiServices::getInstance()->getPageProps();
-		$values = $pageProps->getProperties( $article->mTitle->createFragmentTarget( '' ), 'displaytitle' );
+		$values = $pageProps->getProperties( Title::newFromText( 'DPLTestArticle 1' ), 'displaytitle' );
 		$id = $article->mID;
 
 		var_dump( "Page ID + Title:", "$id: $title" );
