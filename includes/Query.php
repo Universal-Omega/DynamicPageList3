f9dd8216092bcd399465a7a586d263e1967a6808
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-07-12 14:42:37 -0600

    log

--- includes/Query.php
+++ includes/Query.php
@@ -180,6 +180,8 @@ class Query {
 				$query = $this->queryBuilder->getSQL();
 			}
 
+			var_dump( $query )
+
 			if ( Hooks::getDebugLevel() >= 4 && $this->mainConfig->get( MainConfigNames::DebugDumpSql ) ) {
 				$this->sqlQuery = $query;
 			}
