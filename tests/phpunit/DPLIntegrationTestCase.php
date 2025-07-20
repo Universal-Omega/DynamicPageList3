4d2de55a0ae1dbf465ecfe327ce5480ddd3e3f3d
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-07-20 13:11:17 -0600

    Remove

--- tests/phpunit/DPLIntegrationTestCase.php
+++ tests/phpunit/DPLIntegrationTestCase.php
@@ -102,7 +102,6 @@ abstract class DPLIntegrationTestCase extends MediaWikiIntegrationTestCase {
 		];
 
 		$html = $this->runDPLQuery( $params );
-		var_dump( $html );
 		$doc = new DOMDocument();
 		$doc->loadHTML( $html );
 		$queryResults = $doc->getElementById( 'dpl-test-query' );
