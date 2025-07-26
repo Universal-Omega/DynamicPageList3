0e143211125f3bbc6af6c29c463ed369e11a1a88
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-07-25 19:33:55 -0600

    -

--- includes/Heading/OrderedHeading.php
+++ includes/Heading/OrderedHeading.php
@@ -5,6 +5,5 @@ namespace MediaWiki\Extension\DynamicPageList4\Heading;
 class OrderedHeading extends UnorderedHeading {
 
 	protected string $listStart = '<ol%s>';
-
 	protected string $listEnd = '</ol>';
 }
