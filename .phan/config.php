1ed5e5d413d4ec7650b316d90eb77c2991f393f7
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-07-27 19:04:30 -0600

    .

--- .phan/config.php
+++ .phan/config.php
@@ -53,7 +53,7 @@ $cfg['plugins'] = array_merge( $cfg['plugins'], [
 ] );
 
 $cfg['analyze_signature_compatibility'] = true;
-$cfg['enable_class_alias_support'] = false;
+$cfg['enable_class_alias_support'] = true;
 $cfg['enable_extended_internal_return_type_plugins'] = true;
 $cfg['error_prone_truthy_condition_detection'] = true;
 $cfg['redundant_condition_detection'] = true;
