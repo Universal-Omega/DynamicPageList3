13de064bcd3b2e7b3dec02a9b889343de15b6845
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-07-29 20:28:11 -0600

    Fix

--- tests/phpunit/ExternalDomainPatternParserTest.php
+++ tests/phpunit/ExternalDomainPatternParserTest.php
@@ -75,17 +75,17 @@ class ExternalDomainPatternParserTest extends MediaWikiIntegrationTestCase {
 
 		// Domain with wildcard at the beginning separated by `.` from one side
 		yield 'wildcard beginning separated by dot one side' => [
-			'//www.%example.com', 'https://com.%example.www.', '',
+			'//www.%example.com', 'https://com.%example.www.', '/',
 		];
 
 		// Domain with wildcard at the beginning separated by `.` from the other side
 		yield 'wildcard beginning separated by dot other side' => [
-			'//www.%example%.com', 'https://com.%example%.www.', '',
+			'//www.%example%.com', 'https://com.%example%.www.', '/',
 		];
 
 		// Duplicated wildcard doesn't matter
 		yield 'duplicated wildcard' => [
-			'//www.%%example.com', 'https://com.%%example.www.', '',
+			'//www.%%example.com', 'https://com.%%example.www.', '/',
 		];
 	}
 
@@ -109,22 +109,22 @@ class ExternalDomainPatternParserTest extends MediaWikiIntegrationTestCase {
 	public static function provideUnsupportedDomainPattern(): Generator {
 		// We are not supporting `_` as a `.`
 		yield 'underscore not dot' => [
-			'http://www.example_com', 'http://example_com.www.', '',
+			'http://www.example_com', 'http://example_com.www.', '/',
 		];
 
 		// Domain with wildcard in the middle not followed by `.` is not processed
 		yield 'wildcard middle no dot after' => [
-			'//ww%example.com', 'https://com.ww%example.', '',
+			'//ww%example.com', 'https://com.ww%example.', '/',
 		];
 
 		// Multiple wildcards in domain middle, no dot separation
 		yield 'multiple wildcards middle no dot' => [
-			'//%www%example.com', 'https://com.%www%example.', '',
+			'//%www%example.com', 'https://com.%www%example.', '/',
 		];
 
 		// When wildcard should cover `/` we would generate garbage
 		yield 'wildcard covering slash leads to garbage' => [
-			'//%example.%?test=%', 'https://%.%example.', '?test=%',
+			'//%example.%?test=%', 'https://%.%example.', '/?test=%',
 		];
 	}
 }
