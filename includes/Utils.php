c7c0d3adb1f64c253de0d0a8e2c7db2da71ffa3d
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-08-06 18:35:06 -0600

    Move getLogger to Utils

--- includes/Utils.php
+++ includes/Utils.php
@@ -2,7 +2,9 @@
 
 namespace MediaWiki\Extension\DynamicPageList4;
 
+use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\Registration\ExtensionRegistry;
+use Psr\Log\LoggerInterface;
 use function ctype_alnum;
 use function preg_replace;
 use function strlen;
@@ -28,6 +30,10 @@ class Utils {
 		if ( $cat !== '' ) {
 			self::$fixedCategories[$cat] = 1;
 		}
+	}
+
+	public static function getLogger(): LoggerInterface {
+		return LoggerFactory::getInstance( 'DynamicPageList4' );
 	}
 
 	public static function getVersion(): string {
