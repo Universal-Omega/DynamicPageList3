e8dbcc33aa53d0f39f78fbf99d625fe05651f4c9
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-08-08 14:52:42 -0600

    Fix

--- includes/Lister/GalleryList.php
+++ includes/Lister/GalleryList.php
@@ -21,12 +21,8 @@ class GalleryList extends Lister {
 
 	protected function formatItem( Article $article, ?string $pageText ): string {
 		$item = $article->mTitle->getPrefixedText();
-		$itemPageLink = $item;
-		if ( $article->mNamespace === NS_CATEGORY ) {
-			$itemPageLink = ":$item";
-		}
-
 		$this->listAttributes = '';
+
 		$imageWidth = trim( $this->parameters->getParameter( 'imagewidth' ) ?? '' );
 		if ( $imageWidth !== '' ) {
 			$this->listAttributes .= " widths=$imageWidth";
@@ -44,14 +40,19 @@ class GalleryList extends Lister {
 
 		// If PageImages is loaded and this is not a file, attempt to assemble a gallery of PageImages
 		if ( $article->mNamespace !== NS_FILE && ExtensionRegistry::getInstance()->isLoaded( 'PageImages' ) ) {
+			$itemPageLink = "[[$item]]";
+			if ( $article->mNamespace === NS_CATEGORY ) {
+				$itemPageLink = "[[:$item|$item]]";
+			}
+
 			$pageImage = PageImages::getPageImage( $article->mTitle );
 			if ( $pageImage && $pageImage->exists() ) {
 				// Successfully got a page image, wrapping it.
 				$item = $this->getItemStart() . $pageImage->getName() . $this->itemEnd .
-					"[[$itemPageLink|$item]]{$this->itemEnd}link=$item";
+					"$itemPageLink{$this->itemEnd}link=$item";
 			} else {
 				// Failed to get a page image.
-				$item = $this->getItemStart() . $item . $this->itemEnd . "[[$itemPageLink|$item]]";
+				$item = $this->getItemStart() . $item . $this->itemEnd . $itemPageLink;
 			}
 
 			return $this->replaceTagParameters( $item, $article );
